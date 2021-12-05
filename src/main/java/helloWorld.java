e681653bf8d32a1f6b976441d5c022c6bff69de4
Aniranki <marinakirilova0903@gmail.com>
2021-12-05 22:17:58 +0200

    Cganged helloWorld.java

--- src/main/java/helloWorld.java
+++ src/main/java/helloWorld.java
@@ -3,5 +3,6 @@ package main.java;
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello GOIT World");
+        System.out.println("This is my second commit");
     }
 }
